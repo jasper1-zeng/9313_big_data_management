8ebcfde087b0ba695046e0ec25a859045c40f1ad
StarsStation <zengzhengxin1@gmail.com>
2024-11-11 17:05:59 +1100

    P3

--- P3/project3.py
+++ P3/project3.py
@@ -4,15 +4,41 @@ from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+
+
+
+
+
 class project3:
     def run(self, inputpath, outputpath, d, s):
         # List the dealing thoughts that fit the requirements
 
-        # Initialize the SparkContext with the given configuration
+        # 1. Initialize the SparkConf(), SparkContext
 
+        # 2. Extract ID, locations and terms (seperated by #)
+        # input format:
+        """
+        0#(37.141909,98.427776)#aba decides community broadcasting licence
+        1#(52.821524,-91.929612)#kelly disgusted alleged bp ethanol scare
+        2#(-1.274188,-24.540353)#palestinians killed gaza incursion
+        ...
+        """
+
+        # 3. Can use prefix filtering
+            # rank the words in term
+            # calculate the length of prefix using term size and threshold s
         
 
 
+        # 4. Calculate the distance and similarity in the same group if they have the same prefix
+            # First to group, and make sure more than 2 elements
+            # Literate to calculate distance and similarity (make sure each pair just calculate only once)
+
+        
+        # 5. remove duplicate
+            # Can use reducebykey
+        
+        # 6. Rank, output and save the results
 
 
         
